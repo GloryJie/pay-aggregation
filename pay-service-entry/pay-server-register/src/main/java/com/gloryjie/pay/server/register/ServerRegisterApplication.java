d6b33cb4223d6c7f1ec5d86bb74b35cff9a97fd9
GloryJie <gloryjie@163.com>
2019-02-18 21:47:57 +0800

    添加服务注册中心eureka和网关模块

--- pay-service-entry/pay-server-register/src/main/java/com/gloryjie/pay/server/register/ServerRegisterApplication.java
+++ pay-service-entry/pay-server-register/src/main/java/com/gloryjie/pay/server/register/ServerRegisterApplication.java
@@ -1,33 +1,29 @@
 /* ------------------------------------------------------------------
  *   Product:      pay
  *   Module Name:  COMMON
- *   Package Name: com.gloryjie.pay.trade
- *   Date Created: 2018/11/18
+ *   Package Name: com.gloryjie.pay.server.register
+ *   Date Created: 2019/2/18
  * ------------------------------------------------------------------
  * Modification History
  * DATE            Name           Contact
  * ------------------------------------------------------------------
- * 2018/11/18      Jie            GloryJie@163.com
+ * 2019/2/18      Jie            GloryJie@163.com
  */
-package com.gloryjie.pay.trade;
+package com.gloryjie.pay.server.register;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author Jie
  * @since
  */
 @SpringBootApplication
-@ComponentScan("com.gloryjie.pay.*")
-@MapperScan(basePackages = "com.gloryjie.pay.*.dao")
-@EnableDiscoveryClient
-public class PayTradeApplication {
+@EnableEurekaServer
+public class ServerRegisterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PayTradeApplication.class, args);
+        SpringApplication.run(ServerRegisterApplication.class, args);
     }
 }
