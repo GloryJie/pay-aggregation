ecceb357c41c9d46dadd6a15faedbf42fe59d9a9
GloryJie <gloryjie@163.com>
2019-02-21 10:29:33 +0800

    升级spring cloud版本解决gateway不能转发问题

---
+++ pay-service-entry/pay-gateway-entry/src/main/java/com/gloryjie/pay/gateway/GatewayApplication.java
@@ -0,0 +1,29 @@
+/* ------------------------------------------------------------------
+ *   Product:      pay
+ *   Module Name:  COMMON
+ *   Package Name: com.gloryjie.pay.gateway
+ *   Date Created: 2019/2/20
+ * ------------------------------------------------------------------
+ * Modification History
+ * DATE            Name           Contact
+ * ------------------------------------------------------------------
+ * 2019/2/20      Jie            GloryJie@163.com
+ */
+package com.gloryjie.pay.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Jie
+ * @since
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
