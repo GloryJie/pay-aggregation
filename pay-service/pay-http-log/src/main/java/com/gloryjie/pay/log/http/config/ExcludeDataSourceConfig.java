49965a0f605911b2db5ab8afc34f4a1cf38f9d01
GloryJie <GloryJie@163.com>
2019-04-12 15:37:24 +0800

    去除HTTPLog中的事物配置

--- pay-service/pay-http-log/src/main/java/com/gloryjie/pay/log/http/config/ExcludeDataSourceConfig.java
+++ pay-service/pay-http-log/src/main/java/com/gloryjie/pay/log/http/config/ExcludeDataSourceConfig.java
@@ -5,16 +5,18 @@ import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * 排除MySQL数据库配置
+ *
  * @author jie
  * @since 2019/4/8
  */
 @Configuration
 @ConditionalOnProperty(name = "pay.httplog.exclude", havingValue = "mysql")
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DruidDataSourceAutoConfigure.class, MybatisAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DruidDataSourceAutoConfigure.class, MybatisAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
 public class ExcludeDataSourceConfig {
 
 }
