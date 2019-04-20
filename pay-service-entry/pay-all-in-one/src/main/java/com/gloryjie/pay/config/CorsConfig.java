7e26351f5df5ff94896d92fb8372c21a52b67c9f
GloryJie <GloryJie@163.com>
2019-04-20 09:34:08 +0800

    all添加跨域处理

---
+++ pay-service-entry/pay-all-in-one/src/main/java/com/gloryjie/pay/config/CorsConfig.java
@@ -0,0 +1,38 @@
+package com.gloryjie.pay.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+/**
+ * @author jie
+ * @since 2019/4/20
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+
+    @Bean
+    public CorsFilter corsFilter(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+}
--- pay-service-entry/pay-all-in-one/src/main/java/com/gloryjie/pay/config/SecurityConfig.java
+++ pay-service-entry/pay-all-in-one/src/main/java/com/gloryjie/pay/config/SecurityConfig.java
@@ -13,8 +13,11 @@ package com.gloryjie.pay.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -27,6 +30,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  */
 @Configuration
 @EnableWebSecurity
+@Order(-1)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
@@ -44,5 +48,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("super_admin").password(passwordEncoder().encode("123")).roles("user");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.requestMatchers().antMatchers(HttpMethod.OPTIONS, "/**")
+                .and()
+                .cors()
+                .and()
+                .csrf().disable();
     }
 }
